b68c76c1c634573f831d80de62ff0ee1c01e8879
Celeste Torres <ct249896@gmail.com>
2021-12-30 17:18:36 -0500

    mis cambios

---
+++ practicando-js/refuerzo.js
@@ -0,0 +1,30 @@
+//Declaracion de Variables
+
+let vidas = 3;
+let promedio = 20.0;
+let nombre = "Juan";
+
+//Declaren variables partido de deporte de competicion entre dos equipos
+let deporte = "futbol"
+let tiempo = 90;
+let equipos = "Barcelona, Real madrid";
+let tarjetas = "verde, roja, amarilla";
+let jugadores = 22;
+
+let juegos_detenido = true;
+let cantidad_balones = 5;
+
+//Red Social -> publicaciones
+
+let reacciones_disponibles = ["Me gusta","Me divierte", "Me entristece"];
+let correo_electronico = "usuario_78@gmail.com"; //string
+let clave = "usuario1234";
+let url_foto_perfil = "assets.mysocialnetwork.com?=id/assets/img/perfil.png";
+let nombre = "Juan Gutierrez";
+let edad = 20;
+let cant_notificaciones = 0;
+let genero = "Msculino";
+let nmr_amigos = 421;
+
+let estado_cuenta = true;
+let privacidad = ["Solo yo", "Solo amigos", "Publico"];
